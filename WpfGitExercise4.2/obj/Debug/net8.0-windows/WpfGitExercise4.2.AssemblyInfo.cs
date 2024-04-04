7d4f5c7623945692ca4da8634f4c06b26d497d60
MrFaktum <kirtamal@gmail.com>
2024-04-04 16:02:34 +0300

    text and button

--- WpfGitExercise4.2/obj/Debug/net8.0-windows/WpfGitExercise4.2.AssemblyInfo.cs
+++ WpfGitExercise4.2/obj/Debug/net8.0-windows/WpfGitExercise4.2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfGitExercise4.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e6444c2c3774c9ca09ce2a84a4d3559936f589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714aa68806209506cff53883bd489c52f80d1a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfGitExercise4.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfGitExercise4.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
